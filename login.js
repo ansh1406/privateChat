5c1b606b34e9b5008f2d24fd496453b39a447ff9
ansh1406 <122546794+ansh1406@users.noreply.github.com>
2023-10-23 02:27:10 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,6 +1,7 @@
 let loginForm =document.getElementById("loginForm");
 loginForm.addEventListener('submit',async function(event){
+  event.preventDefault();
   username = event.target.getElementsByTagName('input')[0].innerHTML;
   localStorage.setItem('username',username);
-  location.href='/index.html';
+  location.href='index.html';
 });
