77a2e7a6a620d4cf7685cc894277e6e43e74399e
ansh1406 <122546794+ansh1406@users.noreply.github.com>
2023-10-22 21:56:14 +0000

    typing error change

--- login.js
+++ login.js
@@ -1,7 +1,7 @@
 let loginForm =document.getElementById("loginForm");
 loginForm.addEventListener('submit',async function(event){
   event.preventDefault();
-  username = event.target.getElementsByTagName('input')[0].innerHTML;
+  let username = event.target.getElementsByTagName('input')[0].innerHTML;
   localStorage.setItem('username',username);
   location.href='index.html';
 });
