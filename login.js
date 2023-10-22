84f8a2602afe1d3fa43b5dc4cb0dc4b9b93ea7a8
ansh1406 <122546794+ansh1406@users.noreply.github.com>
2023-10-23 02:03:26 +0530

    Create login.js

---
+++ login.js
@@ -0,0 +1,5 @@
+let loginForm =document.getElementById("loginForm");
+loginForm.addEventListener('submit',async function(event){
+  username = event.target.getElementsByTagName('input')[0].innerHTML;
+  localStorage.setItem('username',username);
+});
